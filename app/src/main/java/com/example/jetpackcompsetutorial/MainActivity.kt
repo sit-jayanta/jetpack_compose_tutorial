7914ba8410fa3dbed812b144d368aa807fd26a47
sit-jayanta <jayanta.s@shaligraminfotech.com>
2024-02-19 15:34:45 +0530

    practice in compose

--- app/src/main/java/com/example/jetpackcompsetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompsetutorial/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.ColorRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -19,33 +18,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.jetpackcompsetutorial.ui.theme.JetPackCompseTutorialTheme
-import com.example.jetpackcompsetutorial.ui.theme.Purple80
+import com.example.jetpackcompsetutorial.ui.theme.JetPackComposeTutorialTheme
 import com.example.jetpackcompsetutorial.ui.theme.darkBlue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackCompseTutorialTheme {
+            JetPackComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -86,23 +85,27 @@ Surface(shape = MaterialTheme.shapes.medium , shadowElevation = 15.dp , modifier
                     // Set image size to 40 dp
                     .size(80.dp)
                     // Clip image to be shaped as a circle
-                    .clip(RectangleShape)
+                    .clip(shape = ShapeDefaults.Small)
             )
 
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
 
             Column {
-                Text(text = profile,
-                    fontSize = 25.sp)
+                Text(
+                    text = profile,
+                    fontSize = 25.sp
+                )
                 // Add a vertical space between the author and message texts
                 Spacer(modifier = Modifier.height(5.dp))
                 Text(
                     text = desc,
-                    fontSize = 12.sp,
+                    fontSize = 15.sp,
                     color = darkBlue,
                     style = MaterialTheme.typography.titleSmall
                 )
+                TimerText(viewModel = MyViewModel())
+                GameContent(viewModel = MyViewModel())
             }
         }
     }
@@ -110,13 +113,23 @@ Surface(shape = MaterialTheme.shapes.medium , shadowElevation = 15.dp , modifier
 }
 @Composable
 fun ArtistAvatar() {
-    Box {
-        Image(painter = painterResource(R.drawable.nature), contentDescription = "Artist image",modifier = Modifier
-            // Set image size to 40 dp
-            .size(80.dp)
-            // Clip image to be shaped as a circle
-            .clip(shape = CircleShape))
-        Icon(Icons.Filled.Check, contentDescription = "Check mark")
+    Box(contentAlignment = Alignment.BottomEnd) {
+        Image(
+            painter = painterResource(R.drawable.nature),
+            contentDescription = "Artist image",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(80.dp)
+                // Clip image to be shaped as a circle
+                .clip(shape = CircleShape)
+        )
+        Icon(
+            painterResource(id = R.drawable.remove),
+            contentDescription = "Check mark",
+            modifier = Modifier
+                .height(30.dp)
+                .width(30.dp)
+        )
     }
 }
 
@@ -136,17 +149,38 @@ fun ArtistCardModifiers(
         Card(
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         ) {
-            Image(painter = painterResource(id = R.drawable.geo), contentDescription ="geography" )
         }
     }
 }
 
+@Composable
+fun TimerText(viewModel: MyViewModel) {
+    var timerState by remember {
+        mutableStateOf("")
+    }
+
+    viewModel.subTimerState = {
+        timerState = it.toString()
+    }
+
+    Text(
+        text = timerState
+    )
+}
+
+@Composable
+fun GameContent(viewModel: MyViewModel) {
+    viewModel.updateTimerState()
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetPackCompseTutorialTheme {
-        MessageCard(profile = "Earth", desc = "This is an image of earth or what it should look like")
+    JetPackComposeTutorialTheme {
+        MessageCard(
+            profile = "Earth",
+            desc = "This is an image of earth or what it should look like"
+        )
     }
 }
